453c00529da668a41272c4b9a92ba7999e1fffc3
Shravani <132183348+Shrawebwiz@users.noreply.github.com>
2025-07-16 18:52:01 +0530

    initial commit

--- ExpenxeTracker/src/ExpenseTracker/Main.java
+++ ExpenxeTracker/src/ExpenseTracker/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Ensure the UI uses the system look for a modern feel
+     
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
